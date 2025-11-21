16060f36ced99ed22e316f9c600316d10d176c85
LaurenIsLearning <pleasecontactlauren@gmail.com>
2025-11-21 18:10:55 -0500

    updates

--- demo/demo.bat
+++ demo/demo.bat
@@ -4,7 +4,7 @@ setup.ps1
 @REM #student logs in, creates repo, views repo
 studentLoginRepo.ps1
 
-@REM #student pushes to repo, progress bar of upload, view repo with files after upload
+@REM #student pushes to repo (maybe have it pull file from my flashdrive?), progress bar of upload, view repo with files after upload
 @REM #it would be cool to see what node the data is stored on so i can kill that node for auto healing to see where the data goes
 @REM #it should be replicated, so cool to see where that is too. this might be something for longhorn UI but unsure rn
 studentPush.ps1
--- demo/studentPush.ps1
+++ demo/studentPush.ps1
@@ -1,3 +1,3 @@
-#student pushes to repo, progress bar of upload, view repo with files after upload
+#student pushes to repo(maybe have it pull file from my flashdrive?), progress bar of upload, view repo with files after upload
 #it would be cool to see what node the data is stored on so i can kill that node for auto healing to see where the data goes
 #it should be replicated, so cool to see where that is too. this might be something for longhorn UI but unsure rn
