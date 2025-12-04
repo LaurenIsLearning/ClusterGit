6d501104517fdd067b81c2bafefc0fa0e3aac0ad
Brandon Springer <Brandonspring727@gmail.com>
2025-12-04 07:57:08 -0500

    Final commit

--- ClusterGit-Demo/scripts/3_studentPush.ps1
+++ ClusterGit-Demo/scripts/3_studentPush.ps1
@@ -7,7 +7,6 @@
 
 # ---------- CONFIG ----------
 #If it doesn't work check if you need to put the path to the ssh_config file here
-$env:GIT_SSH_COMMAND = "ssh -F C:/Users/12604/OneDrive/Documents/GitHub/ClusterGit-Demo-Portable/portable/ssh_config"
 
 #Suppresses all ssh warnings
 $env:GIT_SSH_COMMAND = "ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
@@ -54,12 +53,12 @@ for ($i = 1; $i -le 100; $i += 5) {
                     -PercentComplete $i
     Start-Sleep -Milliseconds 40
 }
-fsutil file createnew "$LargeFile" 1 | Out-Null
+fsutil file createnew "$LargeFile" 209715200 | Out-Null #This is a 200Mb file as of right now, but just change the number if you want to test other sizes
 Write-Host -Completed
 Write-Host "File Created!" -ForegroundColor Green
 
 Write-Host ""
-Write-Host "Pushing large file to the cluster (this continues during node failure)..." -ForegroundColor Yellow
+Write-Host "Pushing large file to the cluster..." -ForegroundColor Yellow
 
 git -C "$LocalPath" annex add "$LargeFile" --quiet
 git -C "$LocalPath" commit -m "Add large file" --quiet 2>$null
@@ -67,16 +66,26 @@ git -C "$LocalPath" annex sync --quiet 2>$null
 
 Write-Host "Push Successful!" -ForegroundColor Green
 
+# Copy annex objects to the server bare repo for demo purposes
+#This part is the one that actually uploads the file
+$LocalAnnexObjects = Join-Path $LocalPath ".git\annex\objects"
+$RemoteAnnexObjects = "/srv/git/demo.git/annex/objects"
+
+Write-Host "`nCopying annex objects to server bare repo..." -ForegroundColor Yellow
+scp -r "$LocalAnnexObjects/*" clustergit-pi5-server@10.27.12.244:$RemoteAnnexObjects
+Write-Host "Annex objects copied!" -ForegroundColor Green
+
 # -------------------------------------------
 # CLEAN TREE OUTPUT
 # -------------------------------------------
-Write-Host "`nRepository structure:" -ForegroundColor Cyan
+#Tries to display the file tree of the remote repo but doesn't do it well... Mess with it if you want
+# Write-Host "`nRepository structure:" -ForegroundColor Cyan
 
-$treeLines = & cmd /c "tree `"$LocalPath`" /F"
-$relativeTree = $treeLines -replace [regex]::Escape($LocalPath), "."
-Write-Host $relativeTree
+# $treeLines = & cmd /c "tree `"$LocalPath`" /F"
+# $relativeTree = $treeLines -replace [regex]::Escape($LocalPath), "."
+# Write-Host $relativeTree
 
-Write-Host "`nLarge file upload continues in background..." -ForegroundColor Cyan
+# Write-Host "`nLarge file upload continues in background..." -ForegroundColor Cyan
 
 
 
