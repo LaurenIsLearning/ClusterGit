e4927f983b7716f0c2dcb81ac77f68efbed111cb
ConnerRgit <119899444+ConnerRgit@users.noreply.github.com>
2026-02-06 15:47:02 -0500

    Add AuthProvider to main application structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { AppProvider } from './context/AppContext'
-import { ToastProvider } from './context/ToastContext'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AppProvider } from "./context/AppContext";
+import { ToastProvider } from "./context/ToastContext";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider>
-      <ToastProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ToastProvider>
-    </AppProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <AppProvider>
+        <ToastProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ToastProvider>
+      </AppProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
