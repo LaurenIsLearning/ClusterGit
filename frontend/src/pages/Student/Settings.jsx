4452fa37fbf871249f8c6e9f8047fbbf7a9e3b9b
ConnerRgit <119899444+ConnerRgit@users.noreply.github.com>
2026-02-07 09:54:22 -0500

    Update Settings.jsx

--- frontend/src/pages/Student/Settings.jsx
+++ frontend/src/pages/Student/Settings.jsx
@@ -1,4 +1,4 @@
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Settings() {
   const { user, role, loading } = useAuth();
