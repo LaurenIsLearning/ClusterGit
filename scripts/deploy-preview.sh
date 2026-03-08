d8dc00b477ea81f74e329a9b3103729166737df1
Lauren <robila02@pfw.edu>
2026-03-07 21:12:24 -0500

    added deployment scripts

---
+++ scripts/deploy-preview.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+BRANCH=$(echo "$1" | tr '/' '-' | tr '[:upper:]' '[:lower:]')
+
+echo "Deploying preview for branch: $BRANCH"
+
+TEMPLATE="k8s/preview-template.yaml"
+OUTPUT="/tmp/generated-preview.yaml"
+
+sed "s/{{BRANCH}}/$BRANCH/g" $TEMPLATE > $OUTPUT
+
+kubectl apply -f $OUTPUT
+
+echo ""
+echo "Preview deployed:"
+echo "https://$BRANCH.clustergit.com"
